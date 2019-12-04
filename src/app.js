c47e04043d05649acfd3b6d97b04074faf4129cd
tomergev <tomergev21@gmail.com>
2019-12-04 09:52:03 -0800

    updated config for .env

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const compression = require('compression')
-require('dotenv').config()
+require('dotenv').config({ path: './.env' })
 
 const routes = require('./routes')
 
